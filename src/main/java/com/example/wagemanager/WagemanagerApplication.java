12605e55172db2eeca819d5fc3e046cbdb4ef7ef
Audora <gy2553@naver.com>
2025-11-01 12:47:41 +0900

    feat : 프로젝트 초기파일 생성

---
+++ src/main/java/com/example/wagemanager/WagemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.wagemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wagemanager/WagemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wagemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
